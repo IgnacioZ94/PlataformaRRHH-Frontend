d0dccfc5d6bb084175da0443d7567f95e75d5abe
Ignacio Zayas <bs.zayas@gmail.com>
2024-08-14 19:20:27 -0300

    Diseño, maquetado y consumo de endpoints.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Login from './Components/Login'
 import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
 import Start from './Components/Start'
 import EmployeeLogin from './Components/EmployeeLogin'
+import Dashboard from './Components/Dashboard'
+import Home from './Components/Home'
+import Employee from './Components/Employee'
+import Category from './Components/Category'
+import Profile from './Components/Profile'
+import AddCategory from './Components/AddCategory'
+import PrivateRoute from './Components/PrivateRoute'
 
 function App() {
   return (
@@ -12,6 +19,17 @@ function App() {
       <Route path='/' element={<Start />}></Route>
       <Route path='/adminlogin' element={<Login />}></Route>
       <Route path='/employee_login' element={<EmployeeLogin />}></Route>
+      <Route path='/dashboard' element={
+        <PrivateRoute >
+          <Dashboard />
+        </PrivateRoute>
+      }>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/category' element={<Category />}></Route>
+        <Route path='/dashboard/profile' element={<Profile />}></Route>
+        <Route path='/dashboard/add_category' element={<AddCategory />}></Route>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
